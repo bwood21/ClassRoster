16cb670c69f9d7bf010adc1f834a48a33b53b2ea
Brandon Wood <wood149@purdue.edu>
2017-05-16 15:22:28 -0400

    Add nifty animations

--- ClassRoster.js
+++ ClassRoster.js
@@ -1,4 +1,5 @@
 const items = document.querySelector("form")
+let counter=0;
 const handleSubmit = (ev) =>{
     ev.preventDefault()
     const form = ev.target
@@ -19,15 +20,24 @@ const thirdSubmit = (ex) =>{
     //form.style.color = 'red'
     form.parentNode.style.color = 'red'
     form.parentNode.style.fontWeight = 'bold'
-    var string = 'http://www.getfretless.com/wp-content/uploads/2015/03/Screen-Shot-2014-12-05-at-1.12.10-PM-1024x640.png'
-
-    document.body.style.backgroundImage = "url('"+string+"')"
-    setInterval(animate,100)
+    
+    setInterval(animate,1000)
+   
 
 }
 const animate = (ey) => {
-    document.querySelector
+    if(counter%2){
+        var string = 'http://www.getfretless.com/wp-content/uploads/2015/03/Screen-Shot-2014-12-05-at-1.12.10-PM-1024x640.png'
+    document.body.style.backgroundImage = "url('"+string+"')"
+}
+else {
+    var string = 'https://pbs.twimg.com/profile_images/662759176419020800/LwrBcXs7.png'
+    document.body.style.backgroundImage = "url('"+string+"')"
+}
     
+    counter++
+    speed=speed-100;
+
 }
 
 function addToList (name){
