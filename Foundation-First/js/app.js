a3db6561aca47b7eb53d385721f00d69f3632734
Brandon Wood <wood149@purdue.edu>
2017-05-17 11:58:52 -0400

    Multiple foundation updates

--- Foundation-First/js/app.js
+++ Foundation-First/js/app.js
@@ -1,14 +1,16 @@
 $(document).foundation()
 
 const megaroster = {
+    students: [],
     init(){
         document.querySelector("form#new-student").addEventListener('submit',this.addStudent)
     },
 
     addStudent(ev){
         ev.preventDefault()
-        const studentName = this.studentName.value  //can replace this with ev.target
-        console.log(studentName)
+        const f = ev.target
+        const studentName = this.studentName.value  //can replace 'this' with ev.target
+        
     },
 }
 megaroster.init()
