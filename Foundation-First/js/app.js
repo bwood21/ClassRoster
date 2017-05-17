860b00e64d35ec7709ef9f65ab3c2e6764223ad2
Brandon Wood <wood149@purdue.edu>
2017-05-17 10:35:25 -0400

    Setup submit handler for the form

--- Foundation-First/js/app.js
+++ Foundation-First/js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+const megaroster = {
+    init(){
+        document.querySelector("form#new-student").addEventListener('submit',this.addStudent)
+    },
+
+    addStudent(ev){
+        ev.preventDefault()
+        const studentName = this.studentName.value  //can replace this with ev.target
+        console.log(studentName)
+    },
+}
+megaroster.init()
